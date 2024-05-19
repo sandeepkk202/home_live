c23456f8687fbfe87d8da61a53255513819a3f0c
sandeep_kk <sandeepkk202@gmail.com.com>
2024-05-19 17:24:17 +0530

    first

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    
+    socket.on('stream', (image) => {
+        socket.broadcast.emit('stream', image);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
